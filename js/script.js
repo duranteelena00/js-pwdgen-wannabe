ae10487fafb5c9491a5ce55b6da2bc4865617906
durante.elena00 <durante.elena00@gmail.com>
2021-06-30 17:35:38 +0200

    eseguo l'esercizio

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+//Chiedo all'utente il nome(e lo racchiudo in una variabile)
+var userName = prompt("Inserisci il tuo nome");
+
+//Verifico tramite il console.log che il foglio js sia connesso correttamente
+console.log(userName);
+
+//Chiedo all'utente il cognome
+var userLastName = prompt("Inserisci il tuo cognome");
+console.log(userLastName);
+
+//Chiedo all'utente il colore preferito
+var userFavColor = prompt("Inserisci il tuo colore preferito");
+console.log(userFavColor);
+
+//Recupero l'elemento html in cui inserire del testo
+var passwordPlaceholder = document.getElementById("password");
+console.log(passwordPlaceholder);
+
+//Inserisco il testo nell'elemento html
+passwordPlaceholder.innerText = userName + userLastName +
+userFavColor + "21";
